abc298e90f5f4e2b0df8902755fa91eac6b3373e
Adrian Lee <jun.irok@gmail.com>
2012-01-22 02:36:50 -0500

    using passport but unable to get access token

--- app.js
+++ app.js
@@ -1,87 +1,105 @@
 // Modules
 var express = require('express'),
-	mongoose = require('mongoose'),
-    auth = require('everyauth'),
-	graph = require('fbgraph'),
+	//mongoose = require('mongoose'),
+    passport = require('passport'),
+    FacebookStrategy = require('passport-facebook').Strategy;
+	//util = require('util'),
 	jade = require('jade'),
 	stylus = require('stylus');
 
-var app = express.createServer();
 
 var config = require('./config');
 
+// Passport
+passport.use(new FacebookStrategy({
+	clientID: config.fb.appId,
+	clientSecret: config.fb.appSecret,
+	callbackURL: config.fb.redirect_uri
+	},
+	function (accessToken, refreshToken, profile, done) {
+		// User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+		// 	return done(err, user);
+		// });
+		process.nextTick(function () {
+			console.log(accessToken);
+			return done(null, profile);
+		});
+	})
+);
+
+var app = express.createServer();
+
 // Configuration
-app.configure(function() {
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'jade');
-	app.set('view options', { layout: true });
+app.configure(function () {
 	app.use(express.bodyParser());
     app.use(express.cookieParser());
 	app.use(express.methodOverride());
-	app.use(app.router);
-	app.use(stylus.middleware({ src: __dirname + '/public'}));
-	app.use(express.static(__dirname + '/public'));
     app.use(express.session({ secret: 'asdasdasd'}));
+    
+    app.use(passport.initialize());
+    app.use(passport.session());
+	app.use(app.router);
+
+	app.use(express.static(__dirname + '/public'));
+	app.set('view engine', 'jade');
+	app.set('views', __dirname + '/views');
+	app.set('view options', { layout: true });
+	app.use(stylus.middleware({ src: __dirname + '/public'}));
 });
 
-app.configure('development', function() {
+app.configure('development', function () {
 	app.use(express.errorHandler({ dumpExceptions: true, showStack: true}));
 });
 
-app.configure('production', function() {
+app.configure('production', function () {
 	app.use(express.errorHandler());
 });
 
-
 // Routers
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
 	res.render('index', { title: 'Home' });
 	console.log('GET: index.jade');
 });
 
-app.get('/fb', function(req, res) {
+app.get('/fb', function (req, res) {
 	res.render('fb', { title: 'Facebook'});
 	console.log('GET: fb.jade');
 });
 
-app.get('/fb/auth', function(req, res) {
-	if (!req.query.code) {
-		var authUrl = graph.getOauthUrl ({
-			'client_id': config.fb.appId,
-			'redirect_uri': config.fb.redirect_uri
-		});
-
-		if (!req.query.error) {
-			res.redirect(authUrl);
-		} else {
-			res.send('access denied');
-		}
-		return;
-	}
-
-	graph.authorize({
-		'client_id': config.fb.appId,
-		'client_secret': config.fb.appSecret,
-		'redirect_uri': config.fb.redirect_uri,
-		'code': req.query.code
-	}, function (err, facebookRes) {
-		res.redirect('/');
-		console.log(graph.getAccessToken());
-	});
-});
-
-app.get('/fb/logout', function(req, res) {
-	res.redirect('https://www.facebook.com/logout.php?next=' + config.fb.redirect_uri + '&access_token=' + graph.getAccessToken());
-});
-
-app.get('/jsonp', function(req, res) {
+app.get('/jsonp', function (req, res) {
 	res.render('jsonp', { title: 'JSONP'});
 	console.log('GET: jsonp.jade');
 });
 
-app.get('/mongoose', function(req, res) {
+app.get('/mongoose', function (req, res) {
 	res.render('mongoose', { title: 'MONGOOSE'});
 	console.log('GET: mongoose.jade');
+});
+
+// app.post('/login', passport.authenticate('local'), function(req, res) {
+// 	// if this function gets called, authentication was successful.
+// 	// 'req.user' property contains the authenticated user.
+// });
+
+app.get('/auth/facebook', 
+	passport.authenticate('facebook', { scope: ['user_status', 'user_photos'] }),
+	function ( req, res ) {
+		console.log(req);
+		console.log(res);
+	}
+);
+
+app.get('/auth/facebook/callback',
+	passport.authenticate('facebook', { failureRedirect: '/login' } ),
+	function (req, res) {
+		res.redirect('/');
+	}
+);
+
+app.get('/logout', function (req, res) {
+	console.log('GET: /logout');
+	req.logout();
+	res.redirect('/');
 });
 
 
--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
 	appId: '172509826448',
 	appSecret: '4821346f6d73f32d0af7b5eb29a6acf',
 	scope: 'email, user_about_me, user_birthday, user_location, publish_stream',
-	redirect_uri: 'http://local.host:3000/fb/auth'
+	redirect_uri: 'http://lemonsweat.local:3000/auth/facebook/callback'
     }
 };
