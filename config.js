cb4323f0d01116f22b4eac3326261fe3d7debb10
Adrian Lee <jun.irok@gmail.com>
2012-01-22 02:46:59 -0500

    hide fb app secret credentials

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     fb: {
-	appId: '172509826448',
-	appSecret: '4821346f6d73f32d0af7b5eb29a6acf',
+	appId: process.env.appId,
+	appSecret: process.env.appSecret,
 	scope: 'email, user_about_me, user_birthday, user_location, publish_stream',
 	redirect_uri: 'http://lemonsweat.local:3000/auth/facebook/callback'
     }
